6b107c73ec4ed4c7a0972f19fb9a7a086cb127dc
arjios <antonio.uab.ios.fisica@gmail.com>
2023-09-08 15:26:42 -0300

    usuario, curso entities

---
+++ src/main/java/com/alura/forum/entities/Curso.java
@@ -0,0 +1,74 @@
+package com.alura.forum.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "tb_curso")
+public class Curso {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotEmpty
+	private String nome;
+	@NotEmpty
+	private String categoria;
+	
+	public Curso() {
+	}
+
+	public Curso(Long id, String nome, String categoria) {
+		this.id = id;
+		this.nome = nome;
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Curso other = (Curso) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
